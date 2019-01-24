e83f2550d3349da74ebab862410eb6977698e55e
stoffee <chris@roadkill.org>
2019-01-24 13:25:12 -0800

    fixed git url

--- modules/private-tls-cert/vars.tf
+++ modules/private-tls-cert/vars.tf
@@ -5,44 +5,54 @@
 
 variable "ca_public_key_file_path" {
   description = "Write the PEM-encoded CA certificate public key to this path (e.g. /etc/tls/ca.crt.pem)."
+  default = "../../examples/vault-consul-image/tls/ca.crt.pem"
 }
 
 variable "public_key_file_path" {
   description = "Write the PEM-encoded certificate public key to this path (e.g. /etc/tls/vault.crt.pem)."
+  default = "../../examples/vault-consul-image/tls/vault.crt.pem"
 }
 
 variable "private_key_file_path" {
   description = "Write the PEM-encoded certificate private key to this path (e.g. /etc/tls/vault.key.pem)."
+  default = "../../examples/vault-consul-image/tls/vault.key.pem"
 }
 
 variable "owner" {
   description = "The OS user who should be given ownership over the certificate files."
+  default = "stoffee"
 }
 
 variable "organization_name" {
   description = "The name of the organization to associate with the certificates (e.g. Acme Co)."
+  default = "roadkill.org"
 }
 
 variable "ca_common_name" {
   description = "The common name to use in the subject of the CA certificate (e.g. acme.co cert)."
+  default = "roadkill.org"
 }
 
 variable "common_name" {
   description = "The common name to use in the subject of the certificate (e.g. acme.co cert)."
+  default = "roadkill.org"
 }
 
 variable "dns_names" {
   description = "List of DNS names for which the certificate will be valid (e.g. vault.service.consul, foo.example.com)."
   type        = "list"
+  default     = ["vault.service.consul"]
 }
 
 variable "ip_addresses" {
   description = "List of IP addresses for which the certificate will be valid (e.g. 127.0.0.1)."
   type        = "list"
+  default     = ["127.0.0.1"]
 }
 
 variable "validity_period_hours" {
   description = "The number of hours after initial issuing that the certificate will become invalid."
+  default = "17520"
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
